1b5646b9075171bfd58f1d44f783245c06b8a835
jqshao <jqshao12@gmail.com>
2021-03-26 21:32:47 -0400

    create consts and error

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # beep boop
 ## to dos
 - navbar
-	- give up
+	- sources
+	- about
+	- how to play
 - detect url if it has # meaning that they skip slides
 - change population symbol in counter to red if virus spread
 - play in "portrait mode" not landscape mode popup if on mobile
--- assets/script.js
+++ assets/script.js
@@ -23,43 +23,8 @@ Reveal.initialize({
 });
 
 function commaFormat(value) {
-	return value.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'); // regex to put commas into numbers
+  return value.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'); // regex to put commas into numbers
 }
-
-//checks if user put anything into text
-//edit to allow users to put in space as well/length
-function checkInput() {
-	let newNationName = document.getElementById("inputName").value;
-	const letters = /^[A-Za-z]+$/;
-	if (newNationName.length > 2 && newNationName.length < 15 && newNationName.match(letters)) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-//gets user input name and inserts into span 
-function replaceNationName() {
-	let newNationName = document.getElementById("inputName").value; //gets value of inputName and assigns to newNationName
-	document.querySelectorAll(".nationName").forEach(item => {
-		item.innerText = newNationName;
-	});
-}
-
-function nextSlide() {
-	Reveal.next();
-	console.log("Reveal next, onto: " + Reveal.getSlidePastCount());
-}
-
-function goTo(x) {
-	Reveal.slide(x);
-	console.log("Going to slide: " + x);
-}
-
-function consoleTest(x) {
-	console.log("It worked! " + x);
-}
-
 
 /*https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/hidden hide money counters*/
 
@@ -78,9 +43,49 @@ document.querySelectorAll(".nextPrompt").forEach(item => {
 	item.addEventListener("click", event => nextSlide());
 });
 
+function nextSlide() {
+	Reveal.next();
+	console.log("Reveal next, onto: " + Reveal.getSlidePastCount());
+}
+
+function goTo(x) {
+	Reveal.slide(x);
+	console.log("Going to slide: " + x);
+}
+
+function consoleTest(x) {
+	console.log("It worked! " + x);
+}
+
+//constants for the functions about inserting new nation name
+const submitName = document.getElementById("submitName");
+const inputName = document.getElementById("inputName");
+const errorElement = document.getElementById("error");
+
+//gets user input name and inserts into span 
+function replaceNationName() {
+	let newNationName = inputName.value; //gets value of inputName and assigns to newNationName
+	document.querySelectorAll(".nationName").forEach(item => {
+		item.innerText = newNationName;
+	});
+};
+
+//checks if user put anything into text
+//edit to allow users to put in space as well/length
+function checkInput() {
+	let newNationName = inputName.value;
+	const letters = /^[A-Za-z]+$/;
+	//changing regex to allow for space + alphabet, but no numbers
+	const letterSpaceNoNumbers = /^[a-zA-Z][a-zA-Z\s]*$/;
+	if (newNationName.length > 2 && newNationName.length < 15 && newNationName.match(letterSpaceNoNumbers/*letters*/)) {
+		return true;
+	} else {
+		return false;
+	}
+};
 
 //once the button is clicked, goes to next slide and replaces nation name
-document.getElementById("submitName").addEventListener("click", function() {
+submitName.addEventListener("click", function() {
 	if (checkInput()) {
 		nextSlide();
 		replaceNationName();
@@ -89,4 +94,17 @@ document.getElementById("submitName").addEventListener("click", function() {
 	}
 });
 
-//tells user if they haven't typed anything in
+//tells player that the country name must be between 3-15 chars
+submitName.addEventListener("click", (e) => {
+	let messages = [];
+	//makes general message
+	//problem: doesn't pass the user if they have space in beginning
+	if (checkInput() === false) {
+		messages.push("Nation name must be between 3 to 15 characters without numbers.")
+	}
+
+	if (messages.length > 0) {	
+		e.preventDefault();
+		errorElement.innerText = messages.join(", ");
+	}
+});
